7049753a8d384dffe5aadc14f830e17e7ceb4c25
Sondre Ripegutu <sripegutu@gmail.com>
2019-09-26 22:41:56 +0200

    Added the functions as well

---
+++ functions.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QKeySequence, QPalette, QColor
+from PyQt5.QtCore import Qt
+import sys
+
+### Set dark colors ###
+def set_theme(app):
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.WindowText, Qt.white)
+    palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ToolTipBase, Qt.white)
+    palette.setColor(QPalette.ToolTipText, Qt.white)
+    palette.setColor(QPalette.Text, Qt.white)
+    palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ButtonText, Qt.white)
+    palette.setColor(QPalette.BrightText, Qt.red)
+    palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.HighlightedText, Qt.black)
+    app.setPalette(palette)
+
+
