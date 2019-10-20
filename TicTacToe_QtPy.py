da75379c8ee90683b0f8c3ccaf92e5ccd8ac1ac7
Sondre Ripegutu <sripegutu@gmail.com>
2019-10-20 13:20:03 +0200

    Added dialog windows for viewScore and EnterPlayerIds

--- TicTacToe_QtPy.py
+++ TicTacToe_QtPy.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from PyQt5 import *
 import sys
 import functions as func
 
@@ -8,50 +9,64 @@ import functions as func
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle("Tic Tack Toe")
+
+        self.gameParameters()
         self.initUI()
+        
+    def gameParameters(self):
+        self.player1Score = 0
+        self.player2Score = 0
+        self.player1Name = "Player 1"
+        self.player2Name = "Player 2"
+        print("Setting scores to zero!")
 
     def initUI(self):
         ###Main Menu##
-        mainMenu = QVBoxLayout()
+        main_menu = QVBoxLayout()
         l1 = QLabel()
         l1.setText("Main Menu")
-        mainMenu.addWidget(l1)
-        startGame = QPushButton("Start a Game")
-        mainMenu.addWidget(startGame)
-        enterPlayerIds = QPushButton("Enter Player Ids")
-        mainMenu.addWidget(enterPlayerIds)
-        viewScore = QPushButton("View Score")
-        mainMenu.addWidget(viewScore)
-        exitGame = QPushButton("Exit Game")
-        mainMenu.addWidget(exitGame)
+        main_menu.addWidget(l1)
+        start_game = QPushButton("Start a Game")
+        main_menu.addWidget(start_game)
+        enter_player_ids = QPushButton("Enter Player Ids")
+        main_menu.addWidget(enter_player_ids)
+        view_score = QPushButton("View Score")
+        main_menu.addWidget(view_score)
+        exit_game = QPushButton("Exit Game")
+        main_menu.addWidget(exit_game)
         widget = QWidget()
         self.setCentralWidget(widget)
-        widget.setLayout(mainMenu)
+        widget.setLayout(main_menu)
 
         # Signals for "Main Menu" 
-        startGame.clicked.connect(self.start_game)
-        enterPlayerIds.clicked.connect(self.enter_player_ids)
-        viewScore.clicked.connect(self.view_score)
-        exitGame.clicked.connect(self.close)
+        start_game.clicked.connect(self.startGame)
+        enter_player_ids.clicked.connect(self.enterPlayerIds)
+        view_score.clicked.connect(self.viewScore)
+        exit_game.clicked.connect(self.close)
 
         ##File menu#
         menu = self.menuBar()
-        filemenu = menu.addMenu("&File")
-        about = QAction("&About")
-        filemenu.addAction(about)
-        about.triggered.connect(display_about)
-        close = QAction("&Close")
+        file_menu = menu.addMenu("&File")
+        about = QAction("&About", self)
+        file_menu.addAction(about)
+        about.triggered.connect(self.displayAbout)
+        close = QAction("&Close", self)
         close.triggered.connect(self.close)
-        filemenu.addAction(close)
+        file_menu.addAction(close)
 
-    def start_game(self):
+        self.show()
+
+    def startGame(self):
         print("Start game pushed!")
     
-    def enter_player_ids(self):
-        print("Enter player IDs pushed!")
-    
-    def view_score(self):
-        print("View score pushed!")
+    def enterPlayerIds(self):
+        enterPLayerIds_dlg = EnterPlayerIds(self)
+        enterPLayerIds_dlg.exec_()
+
+    def viewScore(self, s):
+        viewScore_dlg = DisplayScores(self)
+        viewScore_dlg.exec_()
 
     def closeEvent(self,e):
         answer = QMessageBox.question(window, None, "You are about to leave the game.",
@@ -61,55 +76,63 @@ class MainWindow(QMainWindow):
         elif answer & QMessageBox.Cancel:
             e.ignore()
 
-
-def display_about():
-    text = "<center>" \
-           "<h1>Tick Tack Toe</h1>" \
-           "&#8291;" \
-           "</center>" \
-           "<p>This is a game of tic tac toe.  <br/>" \
-           "Version 1.0.0.0<br/>" \
-           "Copyright &copy; Ripegutu Inc.</p>"
-    QMessageBox.about(window, "About Tick Tack Toe", text)
-
-
-# def view_score(player1Score, player2Score):
-#     text = "<center>" \
-#             "<h1>Score</h1>" \
-#             "&#8291;" \
-#            "</center>" \
-#            "<p>Player 1 score is: <br/>" \
-#            "Player 2 score is: </p>" 
-#     QMessageBox.Ok(window, "Game Score", text)
+    def displayAbout(self):
+        text = "<center>" \
+            "<h1>Tick Tack Toe</h1>" \
+            "&#8291;" \
+            "</center>" \
+            "<p>This is a game of tic tac toe.  <br/>" \
+            "Version 1.0.0.0<br/>" \
+            "Copyright &copy; Ripegutu Inc.</p>"
+        QMessageBox.about(window, "About Tick Tack Toe", text)
 
 
+class DisplayScores(QDialog):
+    def __init__(self, parent):
+        super(DisplayScores,self).__init__(parent)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
 
+        self.setWindowTitle("Score View")
+        
+        QBtn = QDialogButtonBox.Ok
+
+        self.score_label = QLabel()
+        score_text = "The score is currently:\n Player 1: " + str(parent.player1Score) +"\n Player 2: " + str(parent.player2Score)
+        self.score_label.setText(score_text)
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.score_label)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+class EnterPlayerIds(QDialog):
+    def __init__(self, parent):
+        super(EnterPlayerIds,self).__init__(parent)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+
+        self.setWindowTitle("Enter player IDs")
+        
+        QBtn = QDialogButtonBox.Ok
+
+        self.ID_label = QLabel()
+        ID_text = "Enter the name for the respective players:"
+        self.ID_label.setText(ID_text)
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.ID_label)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
 
 if __name__ == "__main__":
-    ### Initiate Score of players ###
-    player1Score = 0
-    player2Score = 0
-
     ### Setup GUI ###
     app = QApplication(sys.argv)
-    app.setApplicationName("Tic Tack Toe")
     app.setStyle("Fusion")
     func.set_theme(app)
     window = MainWindow()
-
-    ### File menu ###
-    # menu = window.menuBar().addMenu("&File")
-    # about = QAction("&About")
-    # about.triggered.connect(display_about)
-    # menu.addAction(about)
-    # close = QAction("&Close")
-    # close.triggered.connect(window.close)
-    # menu.addAction(close)
-
-
-
-    ### Signals (MainMenu) ###
-    # viewScore.clicked.connect(lambda x: window.show_score())
-
-    window.show()
     sys.exit(app.exec_())
